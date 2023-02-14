eb3245f4545c2f57d54363fd2527a100a972c9c9
Anatoly <seruderu@gmail.com>
2023-02-14 17:18:47 +0300

    database changes

--- countries/admin.py
+++ countries/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'capital', 'region', 'area')
+    
+admin.site.register(Capital)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Region)
--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+class Capital(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+
+class Region(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+  
+
+class Country(models.Model):
+    name = models.CharField(max_length=30)
+    capital = models.ForeignKey(Capital, on_delete= models.CASCADE)
+    area = models.IntegerField()
+    region = models.ForeignKey(Region, on_delete= models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+
+
+
+
 
