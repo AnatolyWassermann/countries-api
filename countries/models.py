702d86cad5f1401bc8fbfe9076ce6ffe972daea4
Anatoly <seruderu@gmail.com>
2023-02-16 16:11:25 +0300

    changed admin countries look, changed query loop

--- countries/models.py
+++ countries/models.py
@@ -5,15 +5,18 @@ class Region(models.Model):
     area = models.IntegerField(default=1)
     def __str__(self):
         return self.name
+    
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=60, unique=True)
     region = models.ForeignKey(Region, on_delete=models.CASCADE, null=True)
-    capital = models.CharField(max_length=30, unique=True, null=True)
+    capital = models.CharField(max_length=60, null=True)
     area = models.IntegerField(default=1)
     code = models.CharField(max_length=10, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name_plural = "Countries"
--- dbobjects.py
+++ dbobjects.py
@@ -1,5 +1,4 @@
 import json
-import random
 from countries.models import Country, Region
 
 
@@ -7,21 +6,19 @@ file = open('countries.json')
 data = json.load(file)
 
 
-    
 
-random.shuffle(data)
-for i in range(5):
+for i in range(len(data)):
     country_region = data[i]['region']
     country_name = data[i]['name'].get('common')
-    country_capital = "".join(data[i]['capital'])
+    country_capital = ", ".join(data[i]['capital'])
     country_area = data[i]['area']
     country_code = data[i]['cca2']
-
     Region.objects.get_or_create(name=country_region)
-    country = Country.objects.create(name=country_name, capital=country_capital, area=country_area, code=country_code, region=Region.objects.get(name=country_region))
+    country = Country.objects.create(name=country_name, 
+                                     capital=country_capital, 
+                                     area=country_area, 
+                                     code=country_code, 
+                                     region=Region.objects.get(name=country_region))
         
-   
-
-
 
 file.close()
