8a99fef0e90b07cd1f67638b2dfb8b45dc5a76e3
Anatoly <seruderu@gmail.com>
2023-02-16 15:23:58 +0300

    added function for automatic object querying

---
+++ dbobjects.py
@@ -0,0 +1,27 @@
+import json
+import random
+from countries.models import Country, Region
+
+
+file = open('countries.json')
+data = json.load(file)
+
+
+    
+
+random.shuffle(data)
+for i in range(5):
+    country_region = data[i]['region']
+    country_name = data[i]['name'].get('common')
+    country_capital = "".join(data[i]['capital'])
+    country_area = data[i]['area']
+    country_code = data[i]['cca2']
+
+    Region.objects.get_or_create(name=country_region)
+    country = Country.objects.create(name=country_name, capital=country_capital, area=country_area, code=country_code, region=Region.objects.get(name=country_region))
+        
+   
+
+
+
+file.close()
