1e33eb5f7ffb5e89dbf78a6b127e6fb439161887
Anatoly <seruderu@gmail.com>
2023-02-18 19:29:21 +0300

    added drf integerrepresentation

--- countries/admin.py
+++ countries/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Country, Region
 
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'capital', 'region', 'area_km2', 'code', 'updated', 'created']
+    list_display = ['id', 'name', 'capital', 'region', 'area_km2', 'code', 'updated', 'created']
     ''' search function in admin page doesn't work with foreign keys so added __ to foreign key to work properly '''
     search_fields = ['name', 'capital', 'region__name', 'code']
     
@@ -10,6 +10,6 @@ class CountryAdmin(admin.ModelAdmin):
 admin.site.register(Country, CountryAdmin)
 
 class RegionAdmin(admin.ModelAdmin):
-    list_display = ['name', 'area_km2']
+    list_display = ['id', 'name', 'area_km2']
 
 admin.site.register(Region, RegionAdmin)
--- webapi/serializers.py
+++ webapi/serializers.py
@@ -1,20 +1,37 @@
 from rest_framework import serializers
 from countries.models import Country, Region
-from django.utils.formats import number_format
+
+
+
+
+class RegionSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Region
+        fields = ['id', 'name', 'area']
+    
+    
+class ThousandSeparatorField(serializers.IntegerField):
+    def to_representation(self, area):
+        return f"{area:,}"
+    
+# class RegionForeignKeyName(serializers.ReadOnlyField):
+#     def to_representation(self, value):
+#         region = Region.objects.all(id=value)
+#         return region.name
 
 class CountrySerializer(serializers.ModelSerializer):
-    ''' foreign key's name attribute will be shown rather than its ID '''
-    region = serializers.CharField(source='region.name')
-    area = serializers.SerializerMethodField('area_localizer')
+    # ''' foreign key's name attribute will be shown rather than its ID '''
+    # region = serializers.CharField(source='region.name', read_only=True)
+    # region = RegionForeignKeyName(queryset=Region.objects.all())
+    # region = RegionSerializer()
+    
+    area = ThousandSeparatorField()
     class Meta:
         model = Country
         fields = ['id', 'name', 'capital', 'area', 'region', 'code', 'created', 'updated']
 
-    def area_localizer(self, obj):
-        return f"{obj.area:,}"
+    
 
-class RegionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Region
-        fields = ['id', 'name', 'area']
+
 
